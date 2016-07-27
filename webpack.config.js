b0b59fa74061b3cf9f65e818616794730d2aaa29
Christine L. Feaster <feasterc@gmail.com>
2016-07-26 21:46:41 -0700

    Add 'webpack' bundler and 'hjs-webpack' helper
     - Add webpack to bundle modules
     - Add hjs-webpack for setting up webpack with es2015 using Babel and ReactJS hot reloading

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const NODE_ENV = process.env.NODE_ENV;
+
+// Set up server without minification, allow hot reloading
+const isDev = (NODE_ENV === 'development');
+
+// Path variables 
+const root = resolve(__dirname);
+const src = join(root, 'src');
+const dest = join(root, 'dist');
+const modules = join(root, 'node_modules');
+
+const path = require('path'),
+      join = path.join,
+      resolve = path.resolve;
+
+// Require Packages
+const webpack = require('webpack');
+const fs = require('fs');
+const getConfig = require('hjs-webpack');
+
+
+// 'hjs-webpack' webpack starter tool
+// exports a single function that accepts a one argument, an obj
+// that defines config to define a required webpack config.
+// Usage:
+//  in: a single entry file
+//  out: path to dir to generate files
+var config = getConfig({
+    in: join(src, 'app.js'),
+    out: dest,
+    clearBeforeBuild: true
+});
+
+module.exports = config;
