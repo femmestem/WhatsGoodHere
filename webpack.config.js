1d672c035ab9812ffa9fc89ef37d1ac9a2438f32
Christine L. Feaster <feasterc@gmail.com>
2016-09-09 21:33:45 -0700

    Add CSS modules and postcss process configs

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,6 @@ const webpack = require('webpack');
 const fs = require('fs');
 const getConfig = require('hjs-webpack');
 
-
 // 'hjs-webpack' webpack starter tool
 // exports a single function that accepts a one argument, an obj
 // that defines config to define a required webpack config.
@@ -29,6 +28,55 @@ var config = getConfig({
     in: join(src, 'app.js'),
     out: dest,
     clearBeforeBuild: true
+});
+
+// each plugins exported as a function that
+// accepts optional configs, returns a postcss processor
+config.postcss = [].concat([
+    require('precss')({/* options */}),
+    require('autoprefixer')({/* options */}),
+    require('cssnano')({/* options */})
+]);
+
+// CSS Modules
+const cssModulesNames = `${ isDev ? '[path][name]__[local]__' : "" }[hash:base64:5]`;
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+    const found = loaders.filter( l => l && l.loader && l.loader.match(match));
+    if (found) {
+        return found[0];
+    } else {
+        return null;
+    }
+}
+// Existing CSS loader to be modified
+const cssLoader = findLoader(config.module.loaders, matchCssLoaders);
+
+// Clone CSS loader and modify it to only include CSS files in the source dir
+const newLoader = Object.assign(
+    {},
+    cssLoader,
+    {
+        test: /\.module\.css$/,
+        include: [src],
+        loader: cssLoader.loader.replace(
+            matchCssLoaders,
+            `$1$2?modules&localIdentName=${cssModulesNames}$3`
+            )
+    }
+);
+
+// Add modified loader to modules to be bundled
+config.module.loaders.push(newLoader);
+
+// for CSS stylesheets not in souce dir, load without modules support
+cssLoader.test = new RegExp(`[^module]${cssLoader.test.source}`);
+cssLoader.loader = newLoader.loader
+config.module.loaders.push({
+    test: /\.css$/,
+    include: [modules],
+    loader: 'style!css'
 });
 
 module.exports = config;
